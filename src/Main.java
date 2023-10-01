9f5bb7eb4fd04b5ff471741b65776a6dead821cd
Aakash <aakash.20vikki@gmail.com>
2023-10-01 22:08:50 +0530

    added cars to list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Car Class Demo");
+
+        Car c1=new Car(100,20);
+        Car c2=new Car(200,30);
+        Car c3=new Car(300,40);
+        Car c4=new Car(400,50);
+
+        List<Car> carList=new ArrayList<>(Arrays.asList(c1,c2,c3,c4));
+        carList.sort(new CarPriceComparator());
+        System.out.println(carList);
+
+
     }
 }
